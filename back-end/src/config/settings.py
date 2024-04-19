93ac70483543a332917f37022677a14d467e8b15
markus <markus@LocalGPT.myguest.virtualbox.org>
2024-04-19 19:58:17 -0300

    mudei o debug para false no settings

--- back-end/src/config/settings.py
+++ back-end/src/config/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
