812819710e86027b50b7193bc124c2f85dcb0b1e
hahahonline <markusvov@gmail.com>
2024-05-16 17:31:25 +0000

    subindo projeto nest

---
+++ teste/nestjs-typeorm/src/items/dto/update-item.dto.ts
@@ -0,0 +1,7 @@
+
+import { CreateCommentDto } from './create-comment.dto';
+
+export class UpdateItemDto {
+    public: boolean;
+    comments: CreateCommentDto[];
+}
---
+++ teste/nestjs-typeorm/src/items/entities/listing.entity.ts
@@ -0,0 +1,14 @@
+import { AbstractEntity } from "src/database/abstract.entity";
+import { Column, Entity } from "typeorm";
+
+@Entity()
+export class Listing extends AbstractEntity<Listing>{
+
+    @Column()
+    description: string;
+
+    @Column()
+    rating: number;
+
+    
+}
