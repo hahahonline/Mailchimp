b6aab5624a3f52efe0c31ea7d80a7f5ead5bdf4b
Yuri <yuriqpaiva@gmail.com>
2024-05-26 13:38:37 -0300

    Adapta home page para mobile

--- frontend/src/app/(public)/(home)/page.tsx
+++ frontend/src/app/(public)/(home)/page.tsx
@@ -1,10 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { Zap } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Page() {
   return (
-    <section className="flex justify-between items-center gap-4">
+    <section className="flex lg:flex-row flex-col justify-between items-center gap-4 lg:pt-0 pt-20">
       <div className="lg:max-w-[500px] flex flex-col gap-8">
         <h1 className="text-5xl font-bold whitespace-pre-line">
           Da sua mente para o {"\n"} mundo em um clique.
@@ -16,9 +17,11 @@ export default function Page() {
           voz, expanda seu alcance e faça suas mensagens serem ouvidas, onde
           quer que seu público esteja.
         </p>
-        <Button className="max-w-[200px]">
-          <Zap size={24} className="mr-2 h-4 w-4" /> Quero saber mais
-        </Button>
+        <Link href="/cadastro" className="max-w-[200px]">
+          <Button>
+            <Zap size={24} className="mr-2 h-4 w-4" /> Quero saber mais
+          </Button>
+        </Link>
       </div>
       <div>
         <Image
