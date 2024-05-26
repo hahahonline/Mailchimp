3489019fedebe77cdf209ced51f2d443d1fa0f2f
Yuri <yuriqpaiva@gmail.com>
2024-05-26 12:56:16 -0300

    Adiciona melhor estilização à tela de login e Links

--- frontend/src/app/(public)/cadastro/page.tsx
+++ frontend/src/app/(public)/cadastro/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { RegisterForm } from "./components/register-form";
 import Link from "next/link";
+import { ArrowLeft } from "lucide-react";
 
 export default function Page() {
   return (
@@ -12,6 +13,13 @@ export default function Page() {
         height={590}
       />
       <div className="flex flex-col gap-6 lg:max-w-[450px] w-full ml-auto">
+        <Link
+          href="/"
+          className="text-zinc-700 flex items-center gap-2 hover:underline mb-3"
+        >
+          <ArrowLeft size={24} className="text-zinc-700" />
+          Voltar à página inicial
+        </Link>
         <div className="flex flex-col gap-2">
           <h1 className="text-3xl font-semibold">Crie sua conta</h1>
           <p>Junte-se à nossa plataforma de envio de mensagens</p>
@@ -19,7 +27,10 @@ export default function Page() {
         <RegisterForm className="" />
         <span className="text-center text-zinc-800">
           Já possui uma conta?{" "}
-          <Link href="acessar" className="font-semibold text-zinc-900">
+          <Link
+            href="acessar"
+            className="font-semibold text-zinc-900 hover:underline"
+          >
             Fazer login
           </Link>
         </span>
