ff91840fdc1bc2523aeb3fd8d5d2300a83c78ee2
Yuri <yuriqpaiva@gmail.com>
2024-05-30 18:44:06 -0300

    Adiciona tabela de últimas campanhas enviadas

--- frontend/src/app/(private)/dashboard/page.tsx
+++ frontend/src/app/(private)/dashboard/page.tsx
@@ -1,6 +1,7 @@
-import { Users } from "lucide-react";
+import { Send, Users } from "lucide-react";
 import { SummarySection } from "./components/SummarySection";
 import { RecentAddedUsersTable } from "./components/recent-added-users-table";
+import { LastCampaignSentTable } from "./components/last-campaign-sent-table";
 
 export default function Page() {
   return (
@@ -17,6 +18,16 @@ export default function Page() {
         </h2>
         <div className="border rounded-md p-5">
           <RecentAddedUsersTable />
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-3">
+        <h2 className="text-2xl font-semibold flex items-center">
+          <Send className="h-6 w-6 mr-3" strokeWidth={2.3} />
+          Últimas campanhas enviadas
+        </h2>
+        <div className="border rounded-md p-5">
+          <LastCampaignSentTable />
         </div>
       </div>
     </div>
